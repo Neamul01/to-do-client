eaff707f4b3dfb80e4f6370b12b8fb0558eb6756
Neamul01 <neamulhaque2002@gmail.com>
2022-05-18 21:39:43 +0600

    router added and protected route added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import PrivateRoute from './Pages/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='home' element={
+          <PrivateRoute><Home></Home></PrivateRoute>
+        }></Route>
+      </Routes>
     </div>
   );
 }
